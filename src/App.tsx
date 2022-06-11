2fbdd9151f9976c069e6d2b9d611e1ee3db3399a
EnesUnlu <enes9103@gmail.com>
2022-06-12 01:10:02 +0300

    The context structure has been created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import AuthContextProvider from "./context/AuthContext";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div className="App">
+      <AuthContextProvider>
       <AppRouter/>
+      </AuthContextProvider>
     </div>
   );
 }
