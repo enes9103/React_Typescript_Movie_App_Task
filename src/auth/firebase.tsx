669f3844c40acafbe34734f0d2a75164720e60ac
EnesUnlu <enes9103@gmail.com>
2022-06-12 00:17:12 +0300

    .env file was created to keep firebase config information. But config inf. is left as a comment.

--- src/auth/firebase.tsx
+++ src/auth/firebase.tsx
@@ -1,6 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
 
+// In case the firebase config information is kept in the .env file;
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_apiKey,
+//     authDomain: process.env.REACT_APP_authDomain,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId: process.env.REACT_APP_appId,
+//   };
 
 const firebaseConfig = {
     apiKey: "AIzaSyBuPrmxOZWwMfFR6klhrodz5fVvHgRw5J0",
