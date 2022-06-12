976faa02760bd0874846ddd38c09ff51585ec879
EnesUnlu <enes9103@gmail.com>
2022-06-12 12:33:34 +0300

    Data was extracted with axios on the details page.

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const MovieDetail = () => {
+    const { id } = useParams();
+    const [movieDetails, setMovieDetails] = useState();
+
+    // const API_KEY = process.env.REACT_APP_TMDB_KEY;
+    const API_KEY = "eeed018179a4468a4d5a012819757c7d";
+
+    const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
+    const videoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}`;
+    const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+    const defaultImage = "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+    useEffect(() => {
+        axios
+          .get(movieDetailBaseUrl)
+          .then((res) => setMovieDetails(res.data))
+          .catch((err) => console.log(err));
+      }, [movieDetailBaseUrl]);
+      
   return (
     <div>
       
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -7,6 +7,7 @@ import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Main from "../pages/Main";
 import Navbar from "../components/Navbar";
+import MovieDetail from "../pages/MovieDetail";
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,7 @@ const AppRouter = () => {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </Router>
   )
