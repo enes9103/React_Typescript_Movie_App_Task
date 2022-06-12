71a50bf6deaac1c5f2b6dedfc5134e4dafbd01bd
EnesUnlu <enes9103@gmail.com>
2022-06-12 11:03:48 +0300

    Getting data from url was done with axios.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const API_KEY = "eeed018179a4468a4d5a012819757c7d";
+// const API_KEY = process.env.REACT_APP_TMDB_KEY;
+
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+
 
 const Main = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, []);
+
+  const getMovies = (API) => {
+    axios.get(API)
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       Main Page
