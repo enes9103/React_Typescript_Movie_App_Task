e55a36fdd24eaa5ae8eb74f32d1ee5cd6ea6c870
EnesUnlu <enes9103@gmail.com>
2022-06-12 12:08:30 +0300

    Displaying the data in the main page cart components and styling is completed.

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/AuthContext";
 
 const API_KEY = "eeed018179a4468a4d5a012819757c7d";
 // const API_KEY = process.env.REACT_APP_TMDB_KEY;
@@ -9,9 +10,10 @@ const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 
-
 const Main = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     getMovies(FEATURED_API);
@@ -23,8 +25,29 @@ const Main = () => {
       .catch((err) => console.log(err));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm && currentUser) {
+      getMovies(SEARCH_API + searchTerm);
+    } else if (!currentUser) {
+      alert("Please log in to search a movie");
+    } else {
+      alert("Please enter a text");
+    }
+  };
+
   return (
     <div>
+      <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
       <div className="d-flex justify-content-center flex-wrap">
         {movies.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
